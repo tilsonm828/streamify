f369b127ccc81e7b6421034105c267c32f3d6494
tilsonm828 <136986265+tilsonm828@users.noreply.github.com>
2025-11-23 18:53:05 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // Example movie thumbnails
 const m3u8List = {
+    // US news
     cbsn: "https://cbsn-us.cbsnstream.cbsnews.com/out/v1/55a8648e8f134e82a470f83d562deeca/master.m3u8",
     cnn0: "https://turnerlive.warnermediacdn.com/hls/live/586495/cnngo/cnn_slate/VIDEO_4_1064000.m3u8",
     newm: "https://nmxlive.akamaized.net/hls/live/529965/Live_1/index.m3u8",
     bloo: "https://www.bloomberg.com/media-manifest/streams/us.m3u8",
     yaho: "https://d1ewctnvcwvvvu.cloudfront.net/playlist.m3u8",
+    // world news
     cna0: "https://d2e1asnsl7br7b.cloudfront.net/7782e205e72f43aeb4a48ec97f66ebbe/index_4.m3u8",
     alja: "https://live-hls-aje-ak.getaj.net/AJE/01.m3u8",
     nhkw: "https://media-tyo.hls.nhkworld.jp/hls/w/live/master.m3u8",
     trtw: "https://tv-trtworld.medya.trt.com.tr/master.m3u8",
     gnew: "https://live.corusdigitaldev.com/groupa/live/48a5882b-a1ec-42d7-bfd7-6c2739e737da/live.isml/.m3u8",
+    // kids tv
     pbsk: "https://livestream.pbskids.org/out/v1/14507d931bbe48a69287e4850e53443c/est.m3u8",
     flix: "https://stream-us-east-1.getpublica.com/playlist.m3u8?network_id=50&amp;live=1&amp;app_bundle=com.plexapp.desktop",
-    
+    avat: "https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5d0c16d686454ead733d08f8/master.m3u8?advertisingId=&appName=web&appStoreUrl=&appVersion=DNT&app_name=&architecture=&buildVersion=&deviceDNT=0&deviceId=5d0c16d686454ead733d08f8&deviceLat=&deviceLon=&deviceMake=web&deviceModel=web&deviceType=web&deviceVersion=DNT&includeExtendedEvents=false&marketingRegion=US&serverSideAds=false&sid=983&terminate=false&userId=",
+    zomo: "https://amg01553-blueantmediaasi-zoomoonz-samsungnz-rdufn.amagi.tv/playlist/amg01553-blueantmediaasi-zoomoonz-samsungnz/playlist.m3u8",
+    kart: "https://cdn-apse1-prod.tsv2.amagi.tv/linear/amg01076-lightningintern-kartoonchannel-samsungnz/playlist.m3u8",
+    // local news
+    cbsb: "https://cbsn-bos.cbsnstream.cbsnews.com/out/v1/589d66ec6eb8434c96c28de0370d1326/master.m3u8",
+    cbsy: "http://dai.google.com/linear/hls/event/rtcMlf4RTvOEkaudeany5w/master.m3u8",
+    cbss: "https://dai.google.com/linear/hls/event/VE8b4n-YStusEGv5Z2NmsQ/master.m3u8",
+    cbsl: "https://cbsn-la.cbsnstream.cbsnews.com/out/v1/57b6c4534a164accb6b1872b501e0028/master.m3u8",
+    cbsm: "https://cbsn-mia.cbsnstream.cbsnews.com/out/v1/ac174b7938264d24ae27e56f6584bca0/master.m3u8"
 };
 
 // Example movie thumbnails
@@ -35,9 +46,9 @@ const worldNews = [
 const kidsTV = [
     "images/pbsk.png",
     "images/flix.png",
-    // "images/nhkw.png",
-    // "images/trtw.png",
-    // "images/gnew.png"
+    "images/avat.png",
+    "images/zomo.png",
+    "images/kart.png"
 ];
 
 function onThumbnailClick(chName) {
@@ -87,7 +98,9 @@ loadRow("kidsTV");
 
 function startUp() {
     const video = document.getElementById('video');
-    const hlsUrl = 'https://www.bloomberg.com/media-manifest/streams/us.m3u8'; // ← your m3u8 URL
+    // the m3u8 to start the page
+    // const hlsUrl = 'https://service-stitcher.clusters.pluto.tv/stitch/hls/channel/5d0c16d686454ead733d08f8/master.m3u8?advertisingId=&appName=web&appStoreUrl=&appVersion=DNT&app_name=&architecture=&buildVersion=&deviceDNT=0&deviceId=5d0c16d686454ead733d08f8&deviceLat=&deviceLon=&deviceMake=web&deviceModel=web&deviceType=web&deviceVersion=DNT&includeExtendedEvents=false&marketingRegion=US&serverSideAds=false&sid=983&terminate=false&userId=';
+    const hlsUrl = 'https://www.bloomberg.com/media-manifest/streams/us.m3u8';
 
     if (Hls.isSupported()) {
     const hls = new Hls({
